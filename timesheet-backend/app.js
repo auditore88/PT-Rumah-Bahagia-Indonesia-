51c7cb7bd7017306f650bbfe4526f5276a52bd75
auditore88 <ariadiaditya@gmail.com>
2024-06-24 15:45:21 +0700

    sinkron dulu

--- timesheet-backend/app.js
+++ timesheet-backend/app.js
@@ -13,7 +13,7 @@ app.use('/projects', projectRoutes);
 app.use('/timesheets', timesheetRoutes);
 
 sequelize.sync().then(() => {
-    app.listen(3000, () => {
-        console.log('Server is running on port 3000');
+    app.listen(4000, () => {
+        console.log('Server is running on port 4000');
     });
 });
--- timesheet-frontend/src/api.js
+++ timesheet-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://timesheet-backend-rouge.vercel.app'
+    baseURL: 'http://localhost:4000'
 });
 
 export const getProjects = () => api.get('/projects');
