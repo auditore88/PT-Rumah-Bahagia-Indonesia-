ce8249e9d2ddbfb5fee872a1fcf94be5283abe00
auditore88 <ariadiaditya@gmail.com>
2024-06-24 15:16:14 +0700

    deploytest

--- timesheet-frontend/src/api.js
+++ timesheet-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://timesheet-backend-rouge.vercel.app'
 });
 
 export const getProjects = () => api.get('/projects');
